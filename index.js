e44318d567480b63a78bfca3d90b068406e59db8
user-siva <sivag0503@gmail.com>
2022-02-22 20:42:36 +0530

    post functionality added

--- index.js
+++ index.js
@@ -7,17 +7,45 @@ const dotenv = require("dotenv");
 const userRouter = require("./routes/users");
 const authRouter = require("./routes/auth");
 const postRouter = require("./routes/posts");
+const multer = require("multer");
+const bodyParser = require("body-parser");
+const path = require("path");
+
+app.use(bodyParser.urlencoded({ extended: true }));
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL, () => {
   console.log("connected to mongodb");
+  console.log(path.join(__dirname, "public/images"));
 });
 
 //middlewares
+
+app.use("/images", express.static(path.join(__dirname, "public/images")));
+
 app.use(express.json());
 app.use(helmet());
 app.use(morgan("common"));
+
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "public/images");
+  },
+  filename: (req, file, cb) => {
+    cb(null, file.originalname);
+  },
+});
+
+const upload = multer({ storage: storage });
+app.post("/api/upload", upload.single("file"), (req, res) => {
+  try {
+   
+    return res.status(200).json("File uploaded");
+  } catch (error) {
+    console.log(error);
+  }
+});
 
 app.use("/api/users", userRouter);
 app.use("/api/auth", authRouter);
